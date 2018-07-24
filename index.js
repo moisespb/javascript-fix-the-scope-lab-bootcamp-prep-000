849a3d97f9a3be6cbadd5561868069330a1348b2
Moises Perez-Batista <moisesdpbjr@gmail.com>
2018-07-24 06:47:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  
+   const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+ 
 }
 
 var theFunk = funkyFunction("FUNKY!") {
